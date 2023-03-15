fc79a9553936752d111264e776e7383320ee96f2
Treysuarez <treyxsuarez@gmail.com>
2023-03-15 03:16:19 -0400

    added new styling to recipe app

--- food-finderz/src/components/Popular.jsx
+++ food-finderz/src/components/Popular.jsx
@@ -23,10 +23,13 @@ function Popular() {
       <h3>Popular Picks</h3>
 
       <Splide options={{
-        perPage: 3,
+          perPage: 3,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "5rem",
       }}>
-        {popular.
-          map((recipe) => {
+        {popular.map((recipe) => {
             return (
               <SplideSlide>
                 <Card>
@@ -54,11 +57,33 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%
+    bottom: 0%
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `
 
 export default Popular;
