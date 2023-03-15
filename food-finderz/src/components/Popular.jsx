c5721277b246b3bc05ee4056d9ca9b79e7f5159e
Treysuarez <treyxsuarez@gmail.com>
2023-03-15 02:46:10 -0400

    added carosel food options

--- food-finderz/src/components/Popular.jsx
+++ food-finderz/src/components/Popular.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -15,22 +17,27 @@ function Popular() {
   };
 
   return <div>
-    {popular.map((recipe => {
-      return (
-        <Wrapper>
-          <h3>Popular Picks</h3>
-          {popular.map((recipe) => {
-            return(
-              <Card>
-                <p>{recipe.title}</p>
-                <img src ={recipe.image} alt={recipe.title} />
-              </Card>
+
+
+    <Wrapper>
+      <h3>Popular Picks</h3>
+
+      <Splide>
+        {popular.
+          map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
             );
 
           })}
-        </Wrapper>
-      );
-    }))};
+      </Splide>
+    </Wrapper>
+
   </div>
 
 
@@ -43,6 +50,12 @@ const Wrapper = styled.div`
 const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
+  overflow: hidden;
+
+  img{
+    border-radius: 2rem;
+  }
+
 `
 
 export default Popular;
