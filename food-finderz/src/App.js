fc1f37fac765aa2e1535a3ba92ee260c2e6aae1d
Treysuarez <treyxsuarez@gmail.com>
2023-03-16 16:07:41 -0400

    added logo to navigate home

--- food-finderz/src/App.js
+++ food-finderz/src/App.js
@@ -2,11 +2,18 @@ import Pages from "./pages/Pages";
 import Options from "./components/Options";
 import {BrowserRouter} from 'react-router-dom'
 import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiForkKnifeSpoon } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav>
+        <GiForkKnifeSpoon />
+        <Logo to={'/'}>Food Finderz </Logo>
+      </Nav>
       <Search />
       <Options />
       <Pages />
@@ -14,5 +21,21 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 1rem;
+  }
+`
 
 export default App;
