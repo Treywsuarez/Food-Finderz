bbac2b54efc8e6760dfd9373fd6cf0df66363c0f
Shahee05 <shaherun_islam@hotmail.co.uk>
2023-03-21 19:07:28 +0000

    commenting in cuisine

--- food-finderz/src/pages/Cuisine.jsx
+++ food-finderz/src/pages/Cuisine.jsx
@@ -1,10 +1,16 @@
+// For Cuisine, the library is the same, react router for the route
+// framer motion for animation
+// styled components for styles 
+// react for executing codes that happens during the lifecycle - it runs on every render
+
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { Link, useParams } from 'react-router-dom';
 
 
-
+// useParam is another hook, which basically is a parameter which has a named variable passed into a function
+// this declares a route so the React router is rendering the same component, if there is a match 
 function Cuisine() {
 
     const [cuisine, setCuisine] = useState([]);
@@ -21,7 +27,9 @@ function Cuisine() {
         getCuisine(params.option);
         console.log(params.option);
     }, [params.option]);
-
+// Mapping loops the arrays together and by using the arrow function we are going to output something on the screen 
+// i.e. we are rendering the 'item.id' which will output the recipe linked to it. By using it as a key, we create more outputs
+// i.e. attached item.image to display the recipe for visual representation as well as adding an item.title for the name of the recipe
     return ( 
     <Grid>
         {cuisine.map((item) => {
@@ -37,6 +45,8 @@ function Cuisine() {
     </Grid>
 )}
 
+// styling of the page i.e. the cards on display by using grid format (Grid styled.div)
+// this includes the imaging and writing on the cards (they are linked to Card styled.div)
 const Grid = styled.div`
     display: grid;
     grid-template-columns: auto auto auto auto;
