03acb0984b5e8bb132b1f81cb9be173ebf9c6510
Shahee05 <shaherun_islam@hotmail.co.uk>
2023-03-21 19:17:53 +0000

    Home page completed

--- food-finderz/src/pages/Home.jsx
+++ food-finderz/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 // rendering out the components separately helps with the file structure and makes it cleaner
+// The two links we wanted to display on the front page is Veggie Option and Popular Option which would be attractive to a wider audience
 
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
 import Search from "../components/Search";
+// also having a search function, which helps the user search for something directly
 
 
 function Home() {
